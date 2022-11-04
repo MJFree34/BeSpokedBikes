7675631bc17a9f98ea8570118596a363cde1ced7
MJFree34 <mjfree34@gmail.com>
2022-11-03 22:26:47 -0400

    Initial Commit

---
+++ BeSpoked Bikes/BeSpoked_BikesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeSpoked_BikesApp.swift
+//  BeSpoked Bikes
+//
+//  Created by Matt Free on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BeSpoked_BikesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeSpoked Bikes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BeSpoked Bikes
+//
+//  Created by Matt Free on 11/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
